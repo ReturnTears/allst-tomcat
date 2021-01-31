014b2b7ccd9919ca38c84acabd2b59abe6a22e86
returntears <returntears@outlook.com>
2021-01-31 10:51:19 +0800

    模拟webapps部署效果

---
+++ src/main/java/com/lag/edu/server/LagouServlet.java
@@ -0,0 +1,46 @@
+package com.lag.edu.server;
+
+import java.io.IOException;
+
+/**
+ * Step8
+ *
+ * @author June
+ * @since 2021年01月
+ */
+public class LagouServlet extends HttpServlet {
+    @Override
+    public void doGet(Request request, Response response) {
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String content = "<h1>LagServlet get</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doPost(Request request, Response response) {
+        String content = "<h1>LagServlet post</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void destory() throws Exception {
+
+    }
+}
---
+++ webapps/web_demo/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liupengfei54
+  Date: 2020/3/1
+  Time: 16:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
